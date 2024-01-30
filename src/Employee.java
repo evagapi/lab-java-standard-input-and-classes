dc0f5e382cd4fa24e030dd5e818cce1c2d404b17
evagapi <87602505+evagapi@users.noreply.github.com>
2024-01-30 19:40:39 +0100

    feat: create Employee class

---
+++ src/Employee.java
@@ -0,0 +1,51 @@
+public class Employee {
+    private String name;
+    private String email;
+    private int age;
+    private double salary;
+
+    public Employee(String name, String email, int age, double salary) {
+        setName(name);
+        setEmail(email);
+        setAge(age);
+        setSalary(salary);
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    public void setEmail(String email) {
+        if (email.length() < 4) {
+            System.err.println("A valid email is needed");
+        } else {
+            this.email = email;
+        }
+
+    }
+    public void setAge(int age) {
+        if (age < 16 ) {
+            this.age = age;
+            System.out.println("Additional documentation needed");
+        }
+        this.age = age;
+    }
+    public void setSalary(double salary) {
+        if (salary <= 0) {
+            System.err.println("Salary cannot be negative");
+        }
+        this.salary = salary;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+    public String getEmail() {
+        return this.email;
+    }
+    public int getAge() {
+        return this.age;
+    }
+    public double getSalary() {
+        return this.salary;
+    }
+}
