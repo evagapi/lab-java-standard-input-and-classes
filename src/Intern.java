8553f08fc916c3d995850b38d4f26f1dc4ea61c3
evagapi <87602505+evagapi@users.noreply.github.com>
2024-01-30 21:07:33 +0100

    feat: add Intern subclass

--- src/Intern.java
+++ src/Intern.java
@@ -1,2 +1,15 @@
-package PACKAGE_NAME;public class Intern {
+class Intern extends Employee {
+    public static final double MAX_SALARY = 20000.00;
+
+    public Intern(String name, String email, int age, double salary) {
+        super(name, email, age, salary);
+        setSalary(salary);
+    }
+        public void setSalary(double salary) {
+            if (salary > 20000) {
+                throw new IllegalArgumentException("Salary exceeds the limit for an intern");
+            } else {
+                this.salary = salary;
+            }
+        }
 }
